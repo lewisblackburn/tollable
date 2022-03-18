1a55b0ca4a300f672d71defe67fe82ddf0b796f4
lewisblackburn <lewisblackburn10@gmail.com>
2022-03-18 22:24:39 +0000

    fix(updateCounter): uuid -> id

--- app/counters/mutations/updateCounter.ts
+++ app/counters/mutations/updateCounter.ts
@@ -3,16 +3,16 @@ import db from "db"
 import { z } from "zod"
 
 const UpdateCounter = z.object({
-  uuid: z.string(),
+  id: z.string(),
   value: z.number(),
 })
 
 export default resolver.pipe(
   resolver.zod(UpdateCounter),
   resolver.authorize(),
-  async ({ uuid, ...data }) => {
+  async ({ id, ...data }) => {
     // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-    const counter = await db.counter.update({ where: { uuid }, data })
+    const counter = await db.counter.update({ where: { id }, data })
 
     return counter
   }
